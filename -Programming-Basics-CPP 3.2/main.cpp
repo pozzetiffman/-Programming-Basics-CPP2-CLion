a1e2582ff27ad3679018889b7e4c4afec0074e3e
MykytaLvov <mykytalvov@gmail.com>
2024-09-19 20:03:27 +0300

    Было добавлено использовние сетеров и гетеров + новый коментарии к коду

--- -Programming-Basics-CPP 3.2/main.cpp
+++ -Programming-Basics-CPP 3.2/main.cpp
@@ -6,12 +6,12 @@ using namespace std;
 class Counter {
     private:
     int count;  // Счетчик
-    char team;  // Команда ('+', '-', '=' или 'x')
+    string team;  // Команда ('+', '-', '=' или 'x')
     string YesNo;  // Ответ пользователя для указания начального значения счетчика
 
     public:
     // Конструктор по умолчанию, инициализирует начальные значения
-    Counter() : count(0), team(' '), YesNo("да") {}
+    Counter() : count(0), team(" "), YesNo("да") {}
 
     // Конструктор с параметрами, использует ссылку на строку для оптимизации
     Counter(char s, int y, const string& n) {
@@ -21,18 +21,19 @@ class Counter {
     }
 
     // Метод для увеличения счетчика
-    void increment() {count++;}
+    void increment() {setCount(getCount() + 1);} // использование сетера для изменения count не обращаясь на прямую;
 
     // Метод для уменьшения счетчика
-    void decrement() {count--;}
+    void decrement() {setCount(getCount() - 1);} // использование сетера для изменения count не обращаясь на прямую;
 
     // Метод для отображения текущего значения счетчика
-    void display () {cout << "текущие число счетчика: "<< count << endl;}
+    void display () {cout << "текущие число счетчика: "<< getCount() << endl;}// использование гетера для вывода count;
 
     // Метод для ввода команды пользователя
     void inputTeam () {
         cout << "ведите команду ('+', '-', '=' или 'x'): ";
         cin >> team;
+        setTeam(team);
         // Игнорируем оставшиеся символы в строке после ввода
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
@@ -47,6 +48,7 @@ class Counter {
         if (YesNo == "да" || YesNo == "Да" || YesNo == "ДА") {
             cout << "введите начальное значение счётчика: ";
             cin  >> count;
+            setCount(count); // использование сетера для изменения count не обращаясь на прямую;
         }
         else if (YesNo == "нет" || YesNo == "Нет"|| YesNo == "НЕТ") {
             cout << "при отказе начальное значение установлено 0 : \n";
@@ -65,10 +67,10 @@ class Counter {
     void setCount (int Ct) {count = Ct;}
 
     // Метод для получения команды
-    [[nodiscard]] char getTeam () const {return team;}
+    [[nodiscard]] string getTeam () const {return team;}
 
     // Метод для установки команды
-    void setTeam (char Tm) {team = Tm;}
+    void setTeam (string Tm) {team = Tm;}
 
     // Метод для получения строки с ответом "да/нет"
     [[nodiscard]] string getYesNo () const {return YesNo;}
@@ -77,20 +79,20 @@ class Counter {
     void setYesNo (char Yn) {YesNo = Yn;}
 
     // Метод обработки команды
-    bool set_count() {
-        if (team == '+') { // Если команда '+', увеличиваем счетчик
+    bool cekCount() {
+        if (team == "+") { // Если команда '+', увеличиваем счетчик
             increment();
             return true;
         }
-        else if (team == '-') { // Если команда '-', уменьшаем счетчик
+        else if (team == "-") { // Если команда '-', уменьшаем счетчик
             decrement();
             return true;
         }
-        else if (team == '=') { // Если команда '=', отображаем текущее значение счетчика
+        else if (team == "=") { // Если команда '=', отображаем текущее значение счетчика
             display();
             return true;
         }
-        else if (team == 'x' || team == 'X') { // Если команда 'x', завершаем программу
+        else if (team == "x" || team == "X" || team == "х" || team == "Х") { // Если команда 'x', завершаем программу
             cout << "программа завершена";
             return false;
         }
@@ -106,10 +108,11 @@ int main() {
     Counter counter; // Создаем объект класса Counter
     counter.inputCommand(); // Запрашиваем у пользователя начальное значение счетчика
 
+
     // Цикл для ввода команд до тех пор, пока пользователь не введет 'x' для выхода
     while (true) {
         counter.inputTeam(); // Запрашиваем команду
-        if (!counter.set_count()) { // Обрабатываем команду и проверяем, если команда 'x' - выйти из цикла
+        if (!counter.cekCount()) { // Обрабатываем команду и проверяем, если команда 'x' - выйти из цикла
             break;
         }
     }
